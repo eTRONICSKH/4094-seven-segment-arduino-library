7bd8a25fef6670a2f4e1125519e9984b7f9f69f6
theara <theara729@gmail.com>
2020-08-30 16:28:18 +0700

    added character encoding files

--- 4094shiftRegisterSevenSeg.cpp
+++ 4094shiftRegisterSevenSeg.cpp
@@ -1,1 +1,25 @@
-assd
+//4094shiftRegisterSevenSeg.cpp
+
+#include "Arduino.h"
+#include "4094shiftRegisterSevenSeg.h"
+#include "CharacterEncoding.h"
+
+4094shiftRegisterSevenSeg::4094shiftRegisterSevenSeg(uint8_t data_pin, uint8_t clock_pin, uint8_t strobe_pin):_data_pin(data_pin), _clk_pin(clock_pin), _str_pin(strobe_pin){
+  // Default Constructor of EmSevenSegment class
+}
+
+void 4094shiftRegisterSevenSeg::begin(bool type, uint8_t digit){
+	_seg_type 	= type;
+	_digit_num 	= digit;
+	pinMode(_data_pin, OUTPUT);
+	pinMode(_clk_pin, OUTPUT);
+	pinMode(_str_pin, OUTPUT);
+}
+
+void 4094shiftRegisterSevenSeg::shiftOutData(int segmentBuffer[]){
+	digitalWrite(_str_pin, LOW);
+	for(int i=_digit_num-1;i>=0;i--){
+		shiftOut(_data_pin, _clk_pin, MSBFIRST, segChar[segmentBuffer[i]]);
+	}
+	digitalWrite(_str_pin, HIGH);
+}
--- 4094shiftRegisterSevenSeg.h
+++ 4094shiftRegisterSevenSeg.h
@@ -1,1 +1,27 @@
-sdfsdf
+#ifndef 4094shiftRegisterSevenSeg_h
+#define 4094shiftRegisterSevenSeg_h
+
+#include "Arduino.h"
+
+#define CATHODE 1
+#define ANODE 	0
+
+class 4094shiftRegisterSevenSeg{
+public:
+	4094shiftRegisterSevenSeg(uint8_t data_pin, uint8_t clock_pin, uint8_t strobe_pin);
+	void begin(bool type, uint8_t digit);
+
+private:
+	bool 	_seg_type;
+	uint8_t _digit_num;
+	uint8_t _data_pin;
+	uint8_t _clk_pin;
+	uint8_t _str_pin;
+
+	char _main_buffer[];
+
+	void shiftOutData(int segmentBuffer[]);
+
+};
+
+#endif
